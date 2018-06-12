f34d5b12f160f2d05baa463a2f45974425393cd7
Ahsan Siddique <ahsan.siddiq@outlook.com>
2018-06-12 16:32:51 +0500

    Add files via upload

---
+++ ImageSlider/ImageAdapter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.View;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace ImageSlider
+{
+    public class ImageAdapter : PagerAdapter
+    {
+        private Context context;
+        private int[] imageList = 
+        {
+            Resource.Drawable.Ahsan,
+            Resource.Drawable.Ahsan4,
+            Resource.Drawable.Ahsan3,
+        };
+        public ImageAdapter(Context context)
+        {
+            this.context = context;
+        }
+        public override int Count
+        {
+            get
+            {
+                return imageList.Length;
+            }
+        }
+
+        public override bool IsViewFromObject(View view, Java.Lang.Object @object)
+        {
+            return view == ((ImageView)@object);
+        }
+        public override Java.Lang.Object InstantiateItem(View container, int position)
+        {
+            ImageView imageView = new ImageView(context);
+            imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
+            imageView.SetImageResource(imageList[position]);
+            ((ViewPager)container).AddView(imageView, 0);
+            return imageView;
+        }
+
+        public override void DestroyItem(View container, int position, Java.Lang.Object @object)
+        {
+            ((ViewPager)container).RemoveView((ImageView)@object);
+        }
+    }
+}
---
+++ ImageSlider/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V4.View;
+
+namespace ImageSlider
+{
+    [Activity(Label = "ImageSlider", MainLauncher = true , Theme ="@style/MyTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = "Image Slider";
+
+            var viewPager = FindViewById<ViewPager>(Resource.Id.viewPager);
+            ImageAdapter adapter = new ImageAdapter(this);
+            viewPager.Adapter = adapter;
+        }
+    }
+}
+
